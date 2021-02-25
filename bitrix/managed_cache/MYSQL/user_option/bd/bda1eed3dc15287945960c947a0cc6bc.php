e4ca6ef10336c0bca95913f60ab6962ca356e2f2
leo-work <mumkpalt@gmail.com>
2021-02-25 17:05:46 +0700

    add: local template

--- bitrix/managed_cache/MYSQL/user_option/bd/bda1eed3dc15287945960c947a0cc6bc.php
+++ bitrix/managed_cache/MYSQL/user_option/bd/bda1eed3dc15287945960c947a0cc6bc.php
@@ -1,7 +1,7 @@
 <?
 if($INCLUDE_FROM_CACHE!='Y')return false;
-$datecreate = '001614244486';
-$dateexpire = '001614248086';
+$datecreate = '001614245759';
+$dateexpire = '001614249359';
 $ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";a:1:{s:13:"favorite_menu";a:1:{s:5:"stick";s:1:"N";}}}';
 return true;
 ?>
